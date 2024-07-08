8bc628fb7c8157598539e1e0d709f057a086f656
Arisa Rae <playarisa24@gmail.com>
2024-07-09 01:43:05 +0700

    adding input and button with styling

---
+++ masakin/src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import Search from "@/components/Search";
+
+export default function homePage() {
+  return (
+    <>
+      <main className="flex flex-col items-center">
+        <Search></Search>
+      </main>
+    </>
+  );
+}
