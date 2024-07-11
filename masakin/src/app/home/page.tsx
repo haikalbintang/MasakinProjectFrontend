fa682ad62c5c3dd9f190512deeb3496a67f6cd4f
Arisa Rae <playarisa24@gmail.com>
2024-07-11 20:02:47 +0700

    add navbar component, and home, bookmark, my recipe, profile page

---
+++ masakin/src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+
+export default function homePage() {
+  return (
+    <>
+      <main className="flex flex-col pt-[65px] items-center justify-center">
+        <Navbar></Navbar>
+      </main>
+    </>
+  );
+}
--- masakin/src/app/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function LoadingPage() {
-  return <p>Fetching recipe...</p>;
-}
