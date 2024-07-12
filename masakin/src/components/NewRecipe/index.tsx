5bf0b0d39f1173a7160eca3f3f63d86f75d77d16
febianprima <prima@transfez.com>
2024-07-12 23:30:17 +0700

    feat: draggable horizontal scroll experience

--- masakin/src/components/NewRecipe/index.tsx
+++ masakin/src/components/NewRecipe/index.tsx
@@ -1,14 +1,66 @@
+"use client";
+
 import SmallCard from "@/components/RecipeCard/smallCard";
 import { recipeDetailData } from "@/data/Detail";
 import { recipeDetailType } from "@/data/Type";
+import { MouseEvent, useEffect, useRef, useState } from "react";
 
 const NewRecipe = () => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isDragging) {
+      const handleMouseMove = (e: MouseEvent<HTMLDocument>) => {
+        const delta = startX - e.clientX;
+        if (containerRef.current !== null) {
+          containerRef.current.scrollLeft = scrollLeft + delta;
+        }
+      };
+
+      const handleMouseUp = () => {
+        setIsDragging(false);
+      };
+
+      document.addEventListener(
+        "mousemove",
+        handleMouseMove as unknown as EventListener
+      );
+      document.addEventListener("mouseup", handleMouseUp);
+
+      return () => {
+        document.removeEventListener(
+          "mousemove",
+          handleMouseMove as unknown as EventListener
+        );
+        document.removeEventListener("mouseup", handleMouseUp);
+      };
+    }
+  }, [isDragging, startX, scrollLeft]);
+
+  const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDragging(true);
+    setStartX(e.clientX);
+
+    if (containerRef.current !== null) {
+      setScrollLeft(containerRef.current.scrollLeft);
+    }
+  };
+
   return (
     <div className="w-full mb-[52px] pl-[32px]">
       <h2 className="text-[16px] font-[600] text-black mb-[15px]">
         Resep Baru
       </h2>
-      <div className="flex w-auto flex-row space-x-[7px] overflow-x-auto">
+      <div
+        ref={containerRef}
+        className="flex w-auto flex-row space-x-[7px] overflow-x-auto no-scrollbar"
+        onMouseDown={handleMouseDown}
+      >
         {recipeDetailData.map((recipe: recipeDetailType) => (
           <SmallCard key={recipe.id} recipe={recipe} />
         ))}
--- masakin/src/components/SearchBar/index.tsx
+++ masakin/src/components/SearchBar/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -21,18 +21,25 @@ const SearchBar = () => {
             width: "18px",
             height: "18px",
           }}
-          alt="" />
+          alt=""
+        />
         <input
           type="text"
           id="searchInput"
           placeholder="Search recipe"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className="w-full h-[35px] pl-[10px] rounded-[10px] text-[11px] font-[400] placeholder:text-neutral-300 focus:outline-none " />
+          className="w-full h-[35px] pl-[10px] rounded-[10px] text-[11px] font-[400] placeholder:text-neutral-300 focus:outline-none "
+        />
       </div>
-      <button onClick={handleSearch} className="searchButton w-[65px] h-[40px] px-4 rounded-[10px] bg-red text-[15px] font-[400] text-white">Cari</button>
+      <button
+        onClick={handleSearch}
+        className="searchButton w-[65px] h-[40px] px-4 rounded-[10px] bg-red text-[15px] font-[400] text-white"
+      >
+        Cari
+      </button>
     </div>
   );
-}
+};
 
 export default SearchBar;
