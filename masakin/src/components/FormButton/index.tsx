0b6fa8b832977efe0687c813347cab0a79fd66e5
Gustitreshana <gustith2105@gmail.com>
2024-07-15 01:18:05 +0700

    change "type" to required

--- masakin/src/components/FormButton/index.tsx
+++ masakin/src/components/FormButton/index.tsx
@@ -2,14 +2,14 @@
 
 interface FormButtonProps {
     text: string;
-    type?: "button" | "submit";
+    type: "button" | "submit";
     onClick?: () => void;
 }
 
 const FormButton = ({text, onClick, type}: FormButtonProps) => {
     return (
         <div>
-            <button className="w-[280px] h-[54px] rounded-[10px] bg-[#7E9F10] text-[23px] text-[#FFFFFF] hover:bg-[#7E9F10]/80" onClick={onClick}>
+            <button className="w-[280px] h-[54px] rounded-[10px] bg-[#7E9F10] text-[23px] text-[#FFFFFF] hover:bg-[#7E9F10]/80" onClick={onClick} type={type}>
                 {text}
             </button>
     </div>
