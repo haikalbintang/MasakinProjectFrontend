060eeb4357f3dd0b3dd493a42d216f9de2dadf59
haikalbintang <mhaikalbintang.work@gmail.com>
2024-07-17 20:48:09 +0700

    Unimport Reload from layout, add id and name in InputForm. Fix rating with Number().

--- masakin/src/components/InputForm/index.tsx
+++ masakin/src/components/InputForm/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 interface InputFormProps {
+  id: string;
+  name: string;
   label: string;
   type: string;
   placeholder: string;
@@ -9,12 +11,23 @@ interface InputFormProps {
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputForm = ({ label, type, placeholder, className, value, onChange }: InputFormProps) => {
+const InputForm = ({
+  id,
+  name,
+  label,
+  type,
+  placeholder,
+  className,
+  value,
+  onChange,
+}: InputFormProps) => {
   return (
     <div className={`flex flex-col ${className}`}>
       <div className="flex flex-col gap-2">
         <label>{label}</label>
         <input
+          id={id}
+          name={name}
           className="w-[280px] h-[55px] border-[1.5px] rounded-[10px] p-3 border-[#D9D9D9]"
           type={type}
           placeholder={placeholder}
