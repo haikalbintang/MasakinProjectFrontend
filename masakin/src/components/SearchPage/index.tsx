fd3afb5dc89e4dc27df2978d8998d718be667766
haikalbintang <mhaikalbintang.work@gmail.com>
2024-07-18 16:17:29 +0700

    Move /search page to SearchPage component.

---
+++ masakin/src/components/SearchPage/index.tsx
@@ -0,0 +1,13 @@
+// import SearchBar from "@/components/SearchBar";
+// import SearchResult from "@/components/SearchResult";
+
+// export default function HomeSearchPage() {
+//   return (
+//     <>
+//       <main className="flex flex-col pt-[65px] items-center">
+//         <SearchBar></SearchBar>
+//         <SearchResult></SearchResult>
+//       </main>
+//     </>
+//   );
+// }
