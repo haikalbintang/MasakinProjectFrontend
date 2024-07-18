b913988bdd801db95270d320bc8aa5795fdda289
haikalbintang <mhaikalbintang.work@gmail.com>
2024-07-18 16:05:41 +0700

    Comment out /search page.

--- masakin/src/app/search/page.tsx
+++ masakin/src/app/search/page.tsx
@@ -1,13 +1,13 @@
-import SearchBar from "@/components/SearchBar";
-import SearchResult from "@/components/SearchResult";
+// import SearchBar from "@/components/SearchBar";
+// import SearchResult from "@/components/SearchResult";
 
-export default function HomeSearchPage() {
-  return (
-    <>
-      <main className="flex flex-col pt-[65px] items-center">
-        <SearchBar></SearchBar>
-        <SearchResult></SearchResult>
-      </main>
-    </>
-  );
-}
+// export default function HomeSearchPage() {
+//   return (
+//     <>
+//       <main className="flex flex-col pt-[65px] items-center">
+//         <SearchBar></SearchBar>
+//         <SearchResult></SearchResult>
+//       </main>
+//     </>
+//   );
+// }
