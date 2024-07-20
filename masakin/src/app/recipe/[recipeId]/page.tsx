851caadd535f4cfa876a93b81b7405af67b055e0
febianprima <prima@transfez.com>
2024-07-20 14:12:57 +0700

    fix :recipe id crash

--- masakin/src/app/recipe/[recipeId]/page.tsx
+++ masakin/src/app/recipe/[recipeId]/page.tsx
@@ -1,22 +1,44 @@
+"use client";
+
 import RecipeDetail from "@/components/RecipeDetail";
 import BackArrow from "@/components/RecipeDetail/BackArrow";
+import { fetchRecipeById } from "@/data/api";
+import { recipeDetailType, recipeInfo } from "@/data/Type";
+import { useCallback, useEffect, useState } from "react";
 
 interface RecipeDetailPageProps {
   params: { recipeId: string };
 }
 
-export default async function RecipeDetailPage({
-  params,
-}: RecipeDetailPageProps) {
-  const response = await fetch(
-    `https://masakinprojectbe.vercel.app/recipe/${params.recipeId}`
-  );
-  const recipe = await response.json();
+export default function RecipeDetailPage({ params }: RecipeDetailPageProps) {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [recipe, setRecipe] = useState<recipeDetailType | any>();
+
+  const getData = useCallback(async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+      if (token) {
+        const data = await fetchRecipeById(params.recipeId, token);
+        setRecipe(data);
+        // setFilteredData(data);
+      }
+    } catch (err) {
+    } finally {
+    }
+  }, [params.recipeId]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
 
   return (
     <main className="relative">
       <BackArrow />
-      <RecipeDetail recipeDetailData={recipe} />
+      {recipe && !loading ? (
+        <RecipeDetail recipeDetailData={recipe} />
+      ) : (
+        <div />
+      )}
     </main>
   );
 }
